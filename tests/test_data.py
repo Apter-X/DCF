933ebf7d03174e148d1fec6d3cc611b79fadf3c0
Iliass Raihani <43287534+Apter-X@users.noreply.github.com>
2022-06-30 12:30:11 +0100

    Update test_data.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,1 +1,27 @@
 from clean_panda import Data
+
+df = {'name': ['Olivia', 'Dean', 'Alex', 'Jon', 'Tom', 'Jane', 'Kate'],
+      'age': [32, 23, 45, 35, 20, 28, 55],
+      'sex': ['female', 'male', 'male', 'male', 'male', 'female', 'female']}
+
+df2 = {'name': ['Diana', 'Clark', 'Kent'],
+       'age': [32, 23, 45],
+       'sex': ['female', 'male', 'male']}
+
+
+def test_duplicate_data():
+    data = Data(dataframe=df)
+    data.duplicate_data(2)
+    assert data.frame.count().values[0] == 14
+
+
+def test_append_data():
+    data = Data(dataframe=df)
+    data_to_append = Data(dataframe=df2).frame
+    data.append_data([data_to_append])
+    assert data.frame.count().values[0] == 10
+
+
+def test_get_value():
+    data = Data(dataframe=df)
+    assert data.get_value('age', data.frame.name == 'Jane') == 28
