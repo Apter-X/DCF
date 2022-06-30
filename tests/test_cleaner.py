53e9253482bbdc96131822e3acd7131717a0c6ea
Iliass Raihani <43287534+Apter-X@users.noreply.github.com>
2022-06-30 12:30:06 +0100

    Update test_cleaner.py

--- tests/test_cleaner.py
+++ tests/test_cleaner.py
@@ -1,1 +1,24 @@
 from clean_panda import Cleaner
+
+df = {'name': ['Olivia', 'Dean', 'Alex', 'Jon', 'Tom', 'Jane', 'Kate'],
+      'age': [32, 23, 45, 35, 20, 28, 55],
+      'sex': ['female', 'male', 'male', 'male', 'male', 'female', 'female']}
+
+
+def test_normalize_text():
+    clean = Cleaner(dataframe=df)
+    clean.normalize_text(['name', 'sex'])
+    assert clean.data.frame['sex'][4] == 'MALE'
+    assert clean.data.frame['name'][6] == 'KATE'
+
+
+def test_remove_features():
+    clean = Cleaner(dataframe=df)
+    clean.remove_features(['age', 'sex'])
+    assert clean.data.frame.columns == ['name']
+
+
+def test_keep_features():
+    clean = Cleaner(dataframe=df)
+    clean.keep_features(['sex'])
+    assert clean.data.frame.columns == ['sex']
